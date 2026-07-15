688aedddf666e299eb1d6d1be70bee4b90164e40
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-15 11:51:59 +0800

    saya pro

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -2,8 +2,8 @@ kWh = float(input())
 if kWh <= 100:
     totalBill = kWh * 0.3
 else:
-    if kWh < 200:
-        totalBill = 100 * 0.3 + kWh - 100 * 0.5
+    if kWh <= 200:
+        totalBill = 100 * 0.3 + ((kWh - 100) * 0.5)
     else:
-        totalBill = 100 * 0.3 + 100 * 0.5 + kWh - 200 * 0.75
+        totalBill = (100 * 0.3) + (100 * 0.5) + ((kWh - 200) * 0.75)
 print(totalBill)
