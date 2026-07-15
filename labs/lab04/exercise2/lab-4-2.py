86d82d57c693c6fd39d926538bccd4e16589fb8d
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-15 12:26:17 +0800

    c rozly pro maain footbALL

--- labs/lab04/exercise2/lab-4-2.py
+++ labs/lab04/exercise2/lab-4-2.py
@@ -2,7 +2,7 @@ income = float(input())
 if income <= 50000.0:
     totalTax = 0
 else:
-    if totalTax <= 100000.0:
+    if income <= 100000.0:
         totalTax = (50000 * 1/ 100) + ((income - 100000) * 2 / 100)
     else:
         totalTax = ((income - 50000) * 1 / 100)
