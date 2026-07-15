b63d896875168fa8f6adec20f5d115369a8d41f9
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-15 11:48:41 +0800

    sy pro

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -3,7 +3,7 @@ if kWh <= 100:
     totalBill = kWh * 0.3
 else:
     if kWh < 200:
-        totalBill = 100 * 0.3 + kWh * 0.5 - 100
+        totalBill = 100 * 0.3 + kWh - 100 * 0.5
     else:
-        totalBill = 100 * 0.3 + 100 * 0.5 + kWh * 0.75 - 200
+        totalBill = 100 * 0.3 + 100 * 0.5 + kWh - 200 * 0.75
 print(totalBill)
