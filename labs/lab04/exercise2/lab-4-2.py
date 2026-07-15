6e4709b3a22d4e2e7e095a22176699897f036ead
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-15 12:05:47 +0800

    uish sy pro gila sumpah

---
+++ labs/lab04/exercise2/lab-4-2.py
@@ -0,0 +1,9 @@
+income = float(input())
+if income <= 50000.0:
+    totalTax = 0
+else:
+    if income <= 100000.0:
+        totalTax = float(50000 * 1 / 100) + ((income - 100000) * 2 / 100)
+    else:
+        totalTax = (income - 50000) * 1 / 100
+print(totalTax)
